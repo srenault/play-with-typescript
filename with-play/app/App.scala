64971db2867d1fffea2a2260444c4b5b568d9014
Sebastien Renault <sre@zenexity.com>
2013-08-22 23:35:38 +0200

    Assets version is tied to git hash commit

--- with-play/app/App.scala
+++ with-play/app/App.scala
@@ -1,7 +1,10 @@
 package global
 
-object App {
-  val version = "abcd"
-
-  def isProd = play.api.Play.current.mode == play.api.Mode.Prod
+object App extends AppSettings {
+  lazy val version: String = {
+    val maybeFromProp = Option(System.getProperty("GIT_TAG"))
+    val maybeFromEnv = Option(System.getenv("GIT_TAG"))
+    (maybeFromProp orElse maybeFromEnv). getOrElse(GIT_TAG).trim
+  }
+  lazy val isProd = play.api.Play.current.mode == play.api.Mode.Prod
 }
--- with-play/project/Build.scala
+++ with-play/project/Build.scala
@@ -8,14 +8,22 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
   )
 
+  val main = play.Project(appName, appVersion, appDependencies)
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+  .settings(
+    playOnStarted <+= baseDirectory { _ =>
+      { _: java.net.InetSocketAddress => System.setProperty("GIT_TAG", ("git describe --always" !!)); () }
+    }
   )
 
+  .settings(
+    sourceGenerators in Compile <+= (version, sourceManaged in Compile).map {
+      case (gitVersion, sources) =>
+        val file = sources / "AppSettings.scala"
+        IO.write(file, """package global { trait AppSettings { val GIT_TAG = "%s" } }""".format(gitVersion))
+        Seq(file)
+    }
+  )
 }
